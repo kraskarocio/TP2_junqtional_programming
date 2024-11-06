f21e59846898da45037d945e11a485915ff3d436
FacundoArroquy <faarroquy@fi.uba.ar>
2024-11-06 15:58:03 -0300

    [ADD] funcion of add-key and delete

--- src/main/scala/JunqtionalApp.scala
+++ src/main/scala/JunqtionalApp.scala
@@ -3,13 +3,22 @@ import io.circe.*
 import io.circe.parser.*
 import io.circe.syntax.*
 import io.circe.generic.auto.*
-import scala.io.Source 
+import scala.io.Source
+import java.io.*
 
 object JunqtionalApp {
   def printJson(json: Json): Unit = {
     println(json)
   }
 
+  def saveJsonToFile(json: Json, filePath: String): Unit = {
+    val fileWriter = new PrintWriter(new File(filePath))
+    try {
+      fileWriter.write(json.spaces2)
+    } finally {
+      fileWriter.close()
+    }
+  }
   def get(json: Json, path: String): Json = {
     val keys = path.split("\\.").toList
     def getSearch(currentJson: Json, keys: List[String]): Json = keys match {
@@ -26,6 +35,50 @@ object JunqtionalApp {
     }
     getSearch(json, keys)
   }
+  def addKey(json: Json, path: String, key: String, value: Json): Json = {
+    val keys = path.split("\\.").toList
+    def addAtPath(currentJson: Json, keys: List[String]): Json = keys match {
+      case Nil => currentJson
+      case lastKey :: Nil =>
+        currentJson.asObject match {
+          case Some(obj) => obj.add(key, value).asJson
+          case _ => currentJson
+        }
+      case head :: tail =>
+        currentJson.asObject match {
+          case Some(obj) =>
+            val nextJson = obj.toMap.getOrElse(head, Json.obj())
+            val updatedSon = addAtPath(nextJson, tail)
+            obj.add(head, updatedSon).asJson
+        }
+    }
+    addAtPath(json, keys)
+  }
+
+  def delete(json: Json, path: String): Json ={
+    val keys = path.split("\\.").toList
+    def searchToDelete(currentJson: Json, keys: List[String]): Json = keys match {
+      case Nil => currentJson
+      case lastKey :: Nil =>
+        currentJson.asObject match {
+          case Some(obj) => Json.fromJsonObject(obj.remove(lastKey))
+          case None => currentJson
+        }
+      case head :: tail =>
+        currentJson.asObject match {
+          case Some(obj) =>
+            obj.toMap.get(head) match {
+              case Some(nextJson) =>
+                val updatedSon = searchToDelete(nextJson, tail)
+                Json.fromJsonObject(obj.add(head, updatedSon))
+              case None => currentJson
+            }
+          case None => currentJson
+        }
+    }
+  searchToDelete(json, keys)
+  }
+
   def main(args: Array[String]): Unit = {
     val func = "print" // No usamos argumentos de línea de comando
     val path = "resources/EX1.json" // El archivo JSON está en resource
@@ -50,11 +103,27 @@ object JunqtionalApp {
     val json: Either[ParsingFailure, Json] = parse(jsonString)
     json match {
       case Right(parsedJson) =>
-        println(get(parsedJson, "usuarioTriple.nombre"))
+
+  //* Esto es para ir chequeando si andan las funciones. Desp sacarlo
+
+
+        var updatedJson = addKey(parsedJson, "usuario.direccion", "activo", Json.fromBoolean(true))
+        updatedJson = addKey(updatedJson, "usuarioDoble.nombre", "TareasCompletadas", Json.fromInt(5))
+        updatedJson = delete(updatedJson, "usuario.edad")
+        saveJsonToFile(updatedJson, path)
+        println(get(updatedJson, "usuarioTriple.nombre"))
+  // Hasta aca
+
         // if the JSON is valid, choose which function to run based on the first argument
         func match {
-          case "print" => printJson(parsedJson)
-          // Add more functions here ...
+          case "print" => printJson(updatedJson) // Add more functions here ...
+  //esto tambien borrarlo
+            updatedJson = addKey(updatedJson, "usuario.nombre", "edad", Json.fromInt(5))
+            updatedJson = delete(updatedJson, "usuarioDoble.TareasCompletadas")
+            updatedJson = delete(updatedJson, "usuario.activo")
+            saveJsonToFile(updatedJson, path)
+
+  // Hasta aca
           case _ => println(s"ERR: Unknown function '$func'")
         }
       case Left(error) =>
